480c3134076b29d9c9c581bc077801db79a934f2
HippolyteDak <nasdakc@gmail.com>
2026-01-28 23:50:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -294,7 +294,7 @@ wss.on("connection", ws => {
       // clope
       if (room.clope && p.x === room.clope.x && p.y === room.clope.y) {
         p.clopeBonus = true;      // ou autre bonus si tu veux plus tard
-        room.clope = null;
+        //room.clope = null;
       }
       
       // Vérifie si produit sur la case
